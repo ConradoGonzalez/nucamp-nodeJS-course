4dfcff0b2fc98c88a5e63367b4f8f6db04c86d71
ConradoGonzalez <86978884+ConradoGonzalez@users.noreply.github.com>
2021-11-06 12:32:03 -0500

    Week 1 Workshop

--- node-express/routes/campsiteRouter.js
+++ node-express/routes/campsiteRouter.js
@@ -7,6 +7,7 @@ campsiteRouter.route('/')
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
+
 .get((req, res) => {
     res.end('Will send all the campsites to you');
 })
@@ -16,6 +17,27 @@ campsiteRouter.route('/')
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
+})
+.delete((req, res) => {
+    res.end('Deleting all campsites');
+});
+
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+})
+.put((req, res) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on ${req.params.campsiteId}`);
 })
 .delete((req, res) => {
     res.end('Deleting all campsites');
